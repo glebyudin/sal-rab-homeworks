4bd44cdead0ac552e673bdefe471b422d5d5423b
Yudin Gleb <glb.yudin@yandex.ru>
2022-02-08 17:59:10 +0300

    task_1 DONE

--- task_1/solution.js
+++ task_1/solution.js
@@ -1,9 +1,7 @@
 function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
-
     // Задание №1.1. Оповещение о добавлении в корзину
-
     // создайте переменную message
     // присвойте переменной message значение переменной productName
     // добавьте в конец переменной message пробел
@@ -13,36 +11,36 @@ function addInCartMessage(title, price) {
     // добавьте в конец переменной message пробел
     // добавьте в конец переменной message текст 'теперь в корзине!'
     // выведите переменную message в консоль
-
+let message;
+message = productName + ' ' + 'за' + ' ' + productPrice + ' ' + 'теперь в корзине!';
+console.log(message);
     // Конец решения задания №1.1.
-
     return message;
 }
 
 function addInCartCountChange(value) {
-
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
-
     // создайте переменную newValue
     // присвойте ей значение oldValue
     // увеличьте значение newValue на 1
-
+let newValue;
+newValue = oldValue;
+newValue = newValue + 1;
     // Конец решения задания №1.2.
-
     return newValue;
 }
 
 function addInCartSumChange(sum, delta) {
     let oldSum = sum;
     let difference = delta;
-
     // Задание №1.3. Увеличение суммы в корзине
-
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
     // замените склеивание, используемое в newSum Text на шаблонную строку
-
+let newSum = oldSum + difference;
+let newSumText = newSum + ' ₽';
+newSumText = `${newSum} ₽`;
     // Конец решения задания №1.3.
 
     return newSumText;
