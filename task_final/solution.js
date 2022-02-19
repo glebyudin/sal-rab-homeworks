e87d8af3e0f5db61de7d8d6ef7d5018ecc366dd9
Yudin Gleb <glb.yudin@yandex.ru>
2022-02-19 18:18:49 +0300

    task_final DONE

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,25 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
+    let data = {goods: [], order: {}, client: ''
+    };
+ 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
-    }
+    for (let i = 0; i < countOfGoods; i++) {
+        data.goods.push({
+            title: goods[i].title, 
+            count: goods[i].count
+        });
+    };
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
+    data.order = {
+        address: 'ул. ' + address.street + ', дом ' + address.house + ', ' + address.entrance + ' подъезд, ' + address.floor + ' этаж, кв ' + address.flat,
+        sum: sum,
+    };
+    data.client = name + ' ' + phone;
+    let jsonData = JSON.stringify({data: data});
 
     return jsonData;
 }
+
